582b0935d68e80bb3acd349d073b03f832875f9f
fjbarrett <frankebarrett@gmail.com>
2015-06-25 00:41:34 -0700

    Initial Commit

---
+++ BBYScanner/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  BBYScanner
+//
+//  Created by Frank Barrett on 6/25/15.
+//  Copyright (c) 2015 Frank Barrett. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ BBYScannerTests/BBYScannerTests.m
@@ -0,0 +1,40 @@
+//
+//  BBYScannerTests.m
+//  BBYScannerTests
+//
+//  Created by Frank Barrett on 6/25/15.
+//  Copyright (c) 2015 Frank Barrett. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import <XCTest/XCTest.h>
+
+@interface BBYScannerTests : XCTestCase
+
+@end
+
+@implementation BBYScannerTests
+
+- (void)setUp {
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown {
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample {
+    // This is an example of a functional test case.
+    XCTAssert(YES, @"Pass");
+}
+
+- (void)testPerformanceExample {
+    // This is an example of a performance test case.
+    [self measureBlock:^{
+        // Put the code you want to measure the time of here.
+    }];
+}
+
+@end
